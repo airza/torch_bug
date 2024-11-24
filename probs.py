8b8d6381b7fc38df93229b82e99294ae12cff7ab
airza <john@airza.net>
2024-11-24 19:17:32 +0100

    add print statements

--- probs.py
+++ probs.py
@@ -1,10 +1,15 @@
 import torch
-print(torch.__version__)
 probs = torch.load("probs.pt",weights_only=True).to('mps')
-cpuProbs = torch.load("probs.pt",weights_only=True).to('cpu')
+cpuProbs = probs.to('cpu')
 cat = torch.distributions.Categorical(probs)
 cpu_cat = torch.distributions.Categorical(cpuProbs)
+print("entropy")
+print(cat.entropy())
+print(cpu_cat.entropy())
+print("samples")
+#always identical on my machine
 for i in range(10):
     print(cat.sample())
+#(almost) always different, as expected for a somewhat uniform distribution
 for i in range(10):
     print(cpu_cat.sample())
